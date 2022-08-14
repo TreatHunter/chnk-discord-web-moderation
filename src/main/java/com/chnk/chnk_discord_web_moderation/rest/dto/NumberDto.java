0c575b35b2d3eafe247320571a087661edd40ca3
Tsyreniao <wtsofros@gmail.com>
2022-08-14 15:13:34 +0300

    writed class number dto

--- src/main/java/com/chnk/chnk_discord_web_moderation/rest/dto/NumberDto.java
+++ src/main/java/com/chnk/chnk_discord_web_moderation/rest/dto/NumberDto.java
@@ -1,4 +1,20 @@
 package com.chnk.chnk_discord_web_moderation.rest.dto;
 
 public class NumberDto {
+    private Integer number;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "NumberDto{" +
+                "number=" + number +
+                '}';
+    }
 }
