1f7b07fe08e0156b95349650d9aec66895ba6a31
Tsyreniao <wtsofros@gmail.com>
2022-08-28 14:49:43 +0300

    [add] added liquibase, created tables chnk_number and chnk_leet_code_num

--- src/main/java/com/chnk/chnk_discord_web_moderation/entities/ChnkLeetCodeNum.java
+++ src/main/java/com/chnk/chnk_discord_web_moderation/entities/ChnkLeetCodeNum.java
@@ -3,8 +3,10 @@ package com.chnk.chnk_discord_web_moderation.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
+@Table(name = "chnk_leet_code_num")
 @Entity
 public class ChnkLeetCodeNum {
 
